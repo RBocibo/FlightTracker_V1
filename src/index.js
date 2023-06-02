19373ca7494afa269bac908f29a01ffd2997e668
rbocibo <rbocibo@jhb.dvt.co.za>
2023-06-02 09:03:38 +0200

    :fire: Code formatting for below files using Prettier.
    :wrench: index.js
    :wrench: about, contact-us and template.html.
    :wrench: :hammer: webpack.config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import style from "./styles/style.css";
-import image from '../src/images/flight-logo.png';
-import image2 from './scripts/tracking-ongoing-flights.js';
-import { getAllStates } from './scripts/tracking-ongoing-flights';
+import image from "../src/images/flight-logo.png";
+import image2 from "./scripts/tracking-ongoing-flights.js";
+import { getAllStates } from "./scripts/tracking-ongoing-flights";
 
-document.getElementById('buttonListStates').addEventListener('click' , getAllStates);
+document
+  .getElementById("buttonListStates")
+  .addEventListener("click", getAllStates);
--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,33 @@ const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry:{
-        main: path.resolve(__dirname, "./src/index.js"),
-    },
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, "./dist"),
-        clean: true,
-        assetModuleFilename: '[name][ext]'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-        ],
-    },
-    plugins: [
-        new HTMLWebpackPlugin ({
-            title: "Flight Tracker V1",
-            filename: "index.html",
-            template: "src/template.html",
-        }),
-    ]
+  mode: "development",
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Flight Tracker V1",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
 };
